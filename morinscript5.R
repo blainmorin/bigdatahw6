5cebc3e30e4a4f8399120ec90c87820aea646300
blainmorin <blainmorin@brown.edu>
2018-05-12 04:32:19 -0400

    cleaning done

---
+++ morinscript5.R
@@ -0,0 +1,156 @@
+################################################################
+##### Big Data HW 5 ####################################
+##### By Blain Morin ##########################
+############################################
+
+require(readr) || install.packages("readr")
+require(dplyr) || install.packages("dplyr")
+require(caret) || install.packages("caret", dependencies = TRUE)
+require(doParallel) || install.packages("doParallel")
+
+authors = function() {
+  c("Blain Morin")
+}
+
+
+set.seed(100)
+
+Predict_NoShow_Train = read_csv("Predict_NoShow_Train.csv")
+Predict_NoShow_PublicTest_WithoutLabels = read_csv("Predict_NoShow_PublicTest_WithoutLabels.csv")
+Predict_NoShow_PrivateTest_WithoutLabels = read_csv("Predict_NoShow_PrivateTest_WithoutLabels.csv")
+
+factors = c("Gender",
+            "Diabetes",
+            "Alcoholism",
+            "Hypertension",
+            "Handicapped",
+            "Smoker",
+            "Scholarship",
+            "Tuberculosis",
+            "RemindedViaSMS",
+            "DayOfTheWeek",
+            "Status")
+
+
+##################################################
+### Clean Test Data ##############################
+#################################################
+
+
+### First factor Status so that No-Shows are = 1
+Predict_NoShow_Train$Status = ifelse(Predict_NoShow_Train$Status == "No-Show", 1, 0)
+
+### Trim the low count categories (improves gbm predictions)
+Predict_NoShow_Train$Handicapped = ifelse(Predict_NoShow_Train$Handicapped >= 1, 1, 0)
+Predict_NoShow_Train$RemindedViaSMS = ifelse(Predict_NoShow_Train$RemindedViaSMS >= 1, 1, 0)
+Predict_NoShow_Train[factors] = as.data.frame(lapply(Predict_NoShow_Train[factors], factor))
+Predict_NoShow_Train = Predict_NoShow_Train %>% select(-c(DateAppointmentWasMade,
+                                                         DateOfAppointment, ID, Tuberculosis))
+
+### Center and scale continuous predictors (improves gbm predictions)
+Predict_NoShow_Train$Age = scale(Predict_NoShow_Train$Age)
+Predict_NoShow_Train$DaysUntilAppointment = scale(Predict_NoShow_Train$DaysUntilAppointment)
+
+### Create model matrix
+test.preds = model.matrix(Status ~ . , data = Predict_NoShow_Train)
+### Remove intercept column
+test.preds = test.preds[,-1]
+
+### Create outcome vector
+test.out = Predict_NoShow_Train$Status
+
+
+################################################
+### Clean Public Data ##########################
+###############################################
+
+
+### Trim low count categories (to match training data)
+Predict_NoShow_PublicTest_WithoutLabels$Handicapped = ifelse(Predict_NoShow_PublicTest_WithoutLabels$Handicapped >= 1, 1, 0)
+Predict_NoShow_PublicTest_WithoutLabels$RemindedViaSMS = ifelse(Predict_NoShow_PublicTest_WithoutLabels$RemindedViaSMS >= 1, 1, 0)
+Predict_NoShow_PublicTest_WithoutLabels = Predict_NoShow_PublicTest_WithoutLabels %>% 
+  select(-c(DateAppointmentWasMade, DateOfAppointment))
+Predict_NoShow_PublicTest_WithoutLabels[factors[-11]] = 
+  as.data.frame(lapply(Predict_NoShow_PublicTest_WithoutLabels[factors[-11]], factor))
+
+### Center and Scale
+Predict_NoShow_PublicTest_WithoutLabels$Age = scale(Predict_NoShow_PublicTest_WithoutLabels$Age)
+Predict_NoShow_PublicTest_WithoutLabels$DaysUntilAppointment = scale(Predict_NoShow_PublicTest_WithoutLabels$DaysUntilAppointment)
+
+### Create model matrix
+pub.preds = model.matrix(~ . , data = Predict_NoShow_PublicTest_WithoutLabels)
+pub.preds = pub.preds[,-1]
+
+
+############################################
+### Clean Private Data #####################
+###########################################
+
+
+### Trim small and unused categories
+Predict_NoShow_PrivateTest_WithoutLabels$Handicapped = ifelse(Predict_NoShow_PrivateTest_WithoutLabels$Handicapped >= 1, 1, 0)
+Predict_NoShow_PrivateTest_WithoutLabels$RemindedViaSMS = ifelse(Predict_NoShow_PrivateTest_WithoutLabels$RemindedViaSMS >= 1, 1, 0)
+Predict_NoShow_PrivateTest_WithoutLabels = Predict_NoShow_PrivateTest_WithoutLabels %>%
+  select(-c(DateAppointmentWasMade,
+            DateOfAppointment))
+Predict_NoShow_PrivateTest_WithoutLabels[factors[-11]] = 
+  as.data.frame(lapply(Predict_NoShow_PrivateTest_WithoutLabels[factors[-11]], factor))
+
+### Center and scale
+Predict_NoShow_PrivateTest_WithoutLabels$Age = scale(Predict_NoShow_PrivateTest_WithoutLabels$Age)
+Predict_NoShow_PrivateTest_WithoutLabels$DaysUntilAppointment = scale(Predict_NoShow_PrivateTest_WithoutLabels$DaysUntilAppointment)
+
+### Create model matrix
+priv.preds = model.matrix(~ . , data = Predict_NoShow_PrivateTest_WithoutLabels)
+priv.preds = priv.preds[,-1]
+
+
+########################################
+### Tune GBM Model ####################
+######################################
+
+## Set up parallelization 
+cl = makeCluster(4)
+registerDoParallel(cl)
+
+
+
+
+
+
+
+
+stopCluster(cl)
+
+
+
+
+
+preds = predict.train(tester, newdata = Predict_NoShow_PublicTest_WithoutLabels)
+preds2 = predict.train(tester, newdata = Predict_NoShow_PrivateTest_WithoutLabels)
+
+public = as.data.frame(cbind(Predict_NoShow_PublicTest_WithoutLabels$ID, preds))
+private = as.data.frame(cbind(Predict_NoShow_PrivateTest_WithoutLabels$ID, preds2))
+
+write.table(public, file = "public.csv", sep = ",", col.names = FALSE, row.names = FALSE)
+write.table(private, file = "private.csv", sep = ",", col.names = FALSE, row.names = FALSE)
+
+
+
+
+####If no finish
+tester = glm(data = Predict_NoShow_Train,
+            Status ~ Age + Gender + Diabetes + Alcoholism + Hypertension +
+              Handicapped + Smoker + Scholarship + Tuberculosis + RemindedViaSMS +
+              DayOfTheWeek + DaysUntilAppointment, family = binomial(link = "logit"))
+
+predict(tester)
+
+preds = predict(tester, newdata = Predict_NoShow_PublicTest_WithoutLabels)
+preds2 = predict(tester, newdata = Predict_NoShow_PrivateTest_WithoutLabels)
+
+public = as.data.frame(cbind(Predict_NoShow_PublicTest_WithoutLabels$ID, preds))
+private = as.data.frame(cbind(Predict_NoShow_PrivateTest_WithoutLabels$ID, preds2))
+
+write.table(public, file = "public.csv", sep = ",", col.names = FALSE, row.names = FALSE)
+write.table(private, file = "private.csv", sep = ",", col.names = FALSE, row.names = FALSE)
