11a525c183eaeb5253068568eecd729bd9b3c2a8
blain <blainmorin@brown.edu>
2018-05-12 22:05:29 +0000

    added comments

--- morinscript5.R
+++ morinscript5.R
@@ -7,6 +7,8 @@ require(readr) || install.packages("readr")
 require(dplyr) || install.packages("dplyr")
 require(caret) || install.packages("caret", dependencies = TRUE)
 require(doParallel) || install.packages("doParallel")
+require(gbm) || install.packages("gbm", dependencies = TRUE)
+require(nnet) || install.packages("nnet", dependencies = TRUE)
 
 authors = function() {
   c("Blain Morin")
@@ -15,10 +17,13 @@ authors = function() {
 
 set.seed(100)
 
+### Read in training, private test, and public test data
 Predict_NoShow_Train = read_csv("Predict_NoShow_Train.csv")
 Predict_NoShow_PublicTest_WithoutLabels = read_csv("Predict_NoShow_PublicTest_WithoutLabels.csv")
 Predict_NoShow_PrivateTest_WithoutLabels = read_csv("Predict_NoShow_PrivateTest_WithoutLabels.csv")
 
+
+### Identify which columns are factors
 factors = c("Gender",
             "Diabetes",
             "Alcoholism",
@@ -40,14 +45,17 @@ factors = c("Gender",
 ### First factor Status so that No-Shows are = 1
 Predict_NoShow_Train$Status = ifelse(Predict_NoShow_Train$Status == "No-Show", 1, 0)
 
-### Trim the low count categories (improves gbm predictions)
+### Trim the low count categories (improves fit)
 Predict_NoShow_Train$Handicapped = ifelse(Predict_NoShow_Train$Handicapped >= 1, 1, 0)
 Predict_NoShow_Train$RemindedViaSMS = ifelse(Predict_NoShow_Train$RemindedViaSMS >= 1, 1, 0)
 Predict_NoShow_Train[factors] = as.data.frame(lapply(Predict_NoShow_Train[factors], factor))
+
+### Remove non predictors Dates and ID
+### Remove predictors with low influence (Tuberculosis, Day of the Week, Hypertension, Diabetes)
 Predict_NoShow_Train = Predict_NoShow_Train %>% select(-c(DateAppointmentWasMade,
                                                          DateOfAppointment, ID, Tuberculosis, DayOfTheWeek, Hypertension, Diabetes))
 
-### Center and scale continuous predictors (improves gbm predictions)
+### Center and scale continuous predictors (improves fit)
 Predict_NoShow_Train$Age = scale(Predict_NoShow_Train$Age)
 Predict_NoShow_Train$DaysUntilAppointment = scale(Predict_NoShow_Train$DaysUntilAppointment)
 
@@ -109,20 +117,24 @@ priv.preds = priv.preds[,-1]
 ### Tune GBM Model ####################
 ######################################
 
-## Set up parallelization 
-
+### Give levels character names (necessary for caret to run)
 levels(test.out) = c("show", "noshow")
 
+### Initialize parallelization
 cl = makeCluster(detectCores())
 registerDoParallel(cl)
 
+
+### Set training paramters
 objControl <- trainControl(method='cv', number=5, returnResamp='none', summaryFunction = twoClassSummary, classProbs = TRUE)
 
+### Create a list of parameters to check
 gbmgrid = expand.grid(interaction.depth = 1:8, 
                        n.trees = seq(50, 400, by = 50),
                        shrinkage = seq(0, .5, by = .1),
                        n.minobsinnode = seq(1 , 16, by = 5))
 
+### Train the gbm model
 gmbfit = train(x = test.preds, 
                y = test.out, 
                method = "gbm", 
@@ -137,13 +149,15 @@ stopCluster(cl)
 
 
 
-
+### Predict on test sets
 preds = predict.train(gmbfit, newdata = pub.preds, type = "prob")
 preds2 = predict.train(gmbfit, newdata = priv.preds, type = "prob")
 
 public = as.data.frame(cbind(Predict_NoShow_PublicTest_WithoutLabels$ID, preds$noshow))
 private = as.data.frame(cbind(Predict_NoShow_PrivateTest_WithoutLabels$ID, preds2$noshow))
 
+
+### Write csv
 write.table(public, file = "public.csv", sep = ",", col.names = FALSE, row.names = FALSE)
 write.table(private, file = "private.csv", sep = ",", col.names = FALSE, row.names = FALSE)
 
@@ -154,36 +168,55 @@ write.table(private, file = "private.csv", sep = ",", col.names = FALSE, row.nam
 ### Tune NN #########################################
 ####################################################
 
+### Give levels character names (necessary for caret to run)
 levels(test.out) = c("show", "noshow")
 
+### Initialize parallization
 cl = makeCluster(detectCores())
 registerDoParallel(cl)
 
+### Set training parameters
 objControl <- trainControl(method='cv', number=5, returnResamp='none', summaryFunction = twoClassSummary, classProbs = TRUE)
 
-nngrid = expand.grid(interaction.depth = 1:8, 
-                      n.trees = seq(50, 400, by = 50),
-                      shrinkage = seq(0, .5, by = .1),
-                      n.minobsinnode = seq(1 , 16, by = 5))
+### Make list of tuning parameters to check in the NN model
+nngrid = expand.grid(size = 2:12, 
+                      decay = seq(0, 1, by = .1))
 
+
+### Train the model
 nnfit = train(x = test.preds, 
                y = test.out, 
                method = "nnet", 
-               trControl=objControl)
+               trControl=objControl,
+              tuneGrid = nngrid, maxit = 1000)
 
 
 
 stopCluster(cl)
 
 
+### Predict on test sets
 nnpreds = predict.train(nnfit, newdata = pub.preds, type = "prob")
 nnpreds2 = predict.train(nnfit, newdata = priv.preds, type = "prob")
 
 public = as.data.frame(cbind(Predict_NoShow_PublicTest_WithoutLabels$ID, nnpreds$noshow))
 private = as.data.frame(cbind(Predict_NoShow_PrivateTest_WithoutLabels$ID, nnpreds2$noshow))
 
+
+### Write csv
 write.table(public, file = "public.csv", sep = ",", col.names = FALSE, row.names = FALSE)
 write.table(private, file = "private.csv", sep = ",", col.names = FALSE, row.names = FALSE)
+
+
+print(nnfit)
+
+
+
+
+
+
+
+
 
 
 
